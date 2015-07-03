e4909ca57e27a1b82a25137fd39606013bfedaf4
nagizero <nagi.zero+github@gmail.com>
2015-07-03 13:47:52 +0900

    first commit

---
+++ Assets/Rotate.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var speedX: float = 30.0;
+var speedY: float = 40.0;
+var speedZ: float = 50.0;
+
+function Start () {
+
+}
+
+function Update () {
+    var d = Time.deltaTime;
+    Debug.Log(d);
+    transform.Rotate(speedX * d, speedY * d, speedZ * d, Space.Self);
+}
