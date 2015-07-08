8bb5ca2662fb44343793d141aee87a06960a698b
nagizero <nagi.zero+github@gmail.com>
2015-07-08 11:23:18 +0900

    set required parameters

---
+++ Assets/Editor/BuildBatch.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+using System.Collections;
+
+public class BuildBatch : MonoBehaviour {
+	
+	[UnityEditor.MenuItem("Tools/Build Project AllScene Android")]
+	public static void BuildProjectAllSceneAndroid() {
+		EditorUserBuildSettings.SwitchActiveBuildTarget (BuildTarget.Android);
+		string[] allScene = new string[EditorBuildSettings.scenes.Length];
+		int i = 0;
+		foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
+			allScene[i] = scene.path;
+			i++;
+		}
+		PlayerSettings.bundleIdentifier = "com.nagizero.cube";
+		PlayerSettings.statusBarHidden = true;
+		BuildPipeline.BuildPlayer( allScene,
+		                          "cube.apk",
+		                          BuildTarget.Android,
+		                          BuildOptions.None
+		                          );
+	}
+}
